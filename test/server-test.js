60139b7cb030ae2a0dd7e0897296a43e110a8ccf
acareaga <aaroncareaga@gmail.com>
2016-03-01 19:02:51 -0700

    Added request to interact with server

--- test/server-test.js
+++ test/server-test.js
@@ -2,6 +2,18 @@ const assert = require('chai').assert;
 const app = require('../server');
 
 describe('Server', () => {
+  
+  before(done => {
+    this.port = 9876;
+    this.server = app.listen(this.port, (err, result) => {
+      if (err) { return done(err); }
+      done();
+    });
+  });
+
+  after(() => {
+    this.server.close();
+  });
 
   it('should exist', () => {
     assert(app);
