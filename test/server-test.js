2dab79e0847f48087d7a78764f72de542d7b6862
acareaga <aaroncareaga@gmail.com>
2016-03-01 19:08:09 -0700

    Setup test suite for server. Need to create static files for client

--- test/server-test.js
+++ test/server-test.js
@@ -10,6 +10,9 @@ describe('Server', () => {
       if (err) { return done(err); }
       done();
     });
+    this.request = request.defaults({
+      baseUrl: 'http://localhost:9876/'
+    });
   });
 
   after(() => {
@@ -23,9 +26,19 @@ describe('Server', () => {
   describe('GET /', () => {
 
     it('should return a 200', (done) => {
-      request.get('http://localhost:9876', (error, response) => {
+      this.request.get('/', (error, response) => {
         if (error) { done(error); }
         assert.equal(response.statusCode, 200);
+        done();
+      });
+    });
+
+    it('should have a body with the name of the application', (done) => {
+      var title = app.locals.title;
+      this.request.get('/', (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes(title),
+               `"${response.body}" does not include "${title}".`);
         done();
       });
     });
