2781bae807ec463693916885d9203736a840917a
acareaga <aaroncareaga@gmail.com>
2016-03-03 19:12:10 -0700

    Fixed routing error so poll displays. Returns object but need to pull out unique paths

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+var path = require('path');
 const server = http.createServer(app);
 const bodyParser = require('body-parser');
 const votes = {};
+const generateRoutes = require('./lib/generate-routes');
+const generatePoll = require('./lib/generate-poll');
+
 // MAKE SURE SOCKET IO IS BELOW SERVER
 const socketIo = require('socket.io');
 const io = socketIo(server);
 const port = process.env.PORT || 3000;
-
-// create a new poll with custom routes
-const generateRoutes = require('./lib/generate-routes');
-const generatePoll = require('./lib/generate-poll');
 
 if (!module.parent) {
   server.listen(port, function () { console.log('Listening on port ' + port + '.'); });
@@ -21,7 +21,44 @@ if (!module.parent) {
 app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ROUTES
+app.get('/', (request, response) => {
+  response.render(__dirname + '/views/create');
+});
+
+app.post('/poll/', (request, response) => {
+  var sha = generateRoutes.sha();
+  var adminPath = generateRoutes.adminPath(request);
+  var votePath = generateRoutes.votePath(request);
+  var poll = generatePoll.createPoll(sha, request.body)
+  // if (!request.body.poll) { return response.sendStatus(400); }
+  response.render(__dirname + '/views/poll', {
+    admin: adminPath, vote: votePath, poll: poll
+  });
+});
+
+app.get('/vote', (request, response) => {
+  response.render(__dirname + '/views/vote');
+});
+
+app.get('/admin', (request, response) => {
+  response.render(__dirname + '/views/admin');
+});
+
+// app.get('/polls/:id', (request, response) => {
+//   var poll = app.locals.polls[request.params.id];
+//   response.render('poll', { poll: poll });
+// });
+
+// app.post('/polls', (request, response) => {
+//   if (!request.body.poll) { return response.sendStatus(400); }
+//   var id = generateId();
+//   app.locals.polls[id] = request.body.poll;
+//   response.redirect('/polls/' + id);
+// });
 
 // SOCKET IO CONNECTIONS
 io.on('connection', function (socket) {
@@ -50,44 +87,6 @@ io.on('connection', function (socket) {
     io.sockets.emit('userConnection', io.engine.clientsCount);
   });
 });
-
-// ROUTES
-app.get('/', (request, response) => {
-  response.render('create');
-});
-
-app.post('/poll', (request, response) => {
-  if (!request.body.poll) { return response.sendStatus(400); }
-
-  var sha = generateRoutes.sha();
-  var adminPath = generateRoutes.adminPath(request);
-  var votePath = generateRoutes.votePath(request);
-  var poll = generatePoll.createPoll(sha, request.body)
-
-  response.render('/poll/' + sha, {
-    admin: adminPath, vote: votePath, poll: poll
-  });
-});
-
-app.get('/vote', (request, response) => {
-  response.render('vote');
-});
-
-app.get('/admin', (request, response) => {
-  response.render('admin');
-});
-
-// app.get('/polls/:id', (request, response) => {
-//   var poll = app.locals.polls[request.params.id];
-//   response.render('poll', { poll: poll });
-// });
-
-// app.post('/polls', (request, response) => {
-//   if (!request.body.poll) { return response.sendStatus(400); }
-//   var id = generateId();
-//   app.locals.polls[id] = request.body.poll;
-//   response.redirect('/polls/' + id);
-// });
 
 // COUNT VOTES - REFACTOR w/LODASH
 function countVotes(votes) {
